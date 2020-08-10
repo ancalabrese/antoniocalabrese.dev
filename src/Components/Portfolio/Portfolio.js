7d6cba1d3cdd41c151e21486ecfeab9ddc955741
Antonio <antoniobust@gmail.com>
2020-08-10 08:16:28 +0100

    Add ripple effect in portfolio

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Style from './Portfolio.module.css'
 import ComingSoon from '../ComingSoon/ComingSoon'
+import DiscSpinner from '../UI/DiscSpinner/DiscSpinner'
 
 const Portfolio = () => {
     return (
         <div id="portfolio" className={Style['portfolio']}>
             <ComingSoon />
+            <DiscSpinner />
         </div>
     )
 }
