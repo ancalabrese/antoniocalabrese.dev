3e1ddcfa759942274504956c4d1ba2c41239058a
Antonio <antoniobust@gmail.com>
2020-09-06 15:31:19 +0200

    Add home animation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,10 +9,22 @@ const Home = () => {
                 <span className={Style['background-container']} role="img" aria-hidden="true" alt="Antonio Calabrese"> </span>
             </div>
             <div className={Style.text}>
-                <p className={Style.header}>Hi<span>,</span></p>
-                <p className={Style.header}> I<span>'</span>M ANTONIO<span>.</span></p>
-                <p className={Style['sub-header']}>SOFTWARE DEVELOPER<span>.</span></p>
-                <p className={Style['sub-header']}>SURFER<span>.</span></p>
+                <p className={Style.header}>
+                    <span id={Style.H} className={[Style.animated, Style['slide-in-right']].join(' ')}>H</span><span id={Style.I} className={[Style['animated-fast'], Style['slide-in-left']].join(' ')}>i<span className={Style.highlight}>,</span></span>
+                </p>
+                <p className={Style.header}>
+                    <span id={Style.name} className={[Style['animated-fast'], Style['slide-in-right']].join(' ')}>
+                        I<span className={Style.highlight}>'</span>M ANTONIO<span className={Style.highlight}>.</span>
+                    </span>
+                </p>
+                <p className={Style['sub-header']}>
+                    <span id={Style['first-sub-header']} className={[Style['animated-fast'], Style['slide-in-left']].join(' ')}>
+                        SOFTWARE DEVELOPER<span className={Style.highlight}>.</span>
+                    </span>
+                </p>
+                <p className={Style['sub-header']}>
+                    <span id={Style['second-sub-header']} className={[Style['animated-fast'], Style['slide-in-left']].join(' ')}>SURFER<span className={Style.highlight}>.</span></span>
+                </p>
             </div>
             <div className={Style.signature}>
                 <img src={signature} alt="Signature" />
