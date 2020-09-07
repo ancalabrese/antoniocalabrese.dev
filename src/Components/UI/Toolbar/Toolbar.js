edb49f5396f140164f44cb4b0ff307d546086339
Antonio <antoniobust@gmail.com>
2020-09-07 13:37:21 +0200

    Customize favicon

--- src/Components/UI/Toolbar/Toolbar.js
+++ src/Components/UI/Toolbar/Toolbar.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Style from './Toolbar.module.css'
 import mainStyle from '../../../mainStyle.module.css'
 import Navigation from '../Navigation/Navigation'
-import logo from '../../../Assets/pic1.jpg'
-
+import logo from '../../../Assets/acLogo.png'
 
 const Toolbar = (props) => {
     return (
