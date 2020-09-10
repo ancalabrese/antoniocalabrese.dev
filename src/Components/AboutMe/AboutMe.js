22910c56b7f32dab85371623f3fe363c8dbf1264
Antonio <antoniobust@gmail.com>
2020-09-10 18:01:06 +0100

    Styled about me

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,10 +4,20 @@ import Style from './AboutMe.module.css'
 const AboutMe = (props) => {
     return (
         <div id="about-me" className={Style['about-me']}>
-            <p><b>Hello World!! :)</b></p>
-            <p>I'm Antonio, I was born {new Date().getFullYear() - 1991} years ago in the sunnny Apulia, Italy &#127774;🏖 currently based in London.</p>
-            <p>I like creating stuff, whether is with code or bolts and screws.</p>
-            <p>I hope you enjoy my projects, if you wish to talk or contribute <a href="#contacts">connect with me!</a></p>
+            <span className={[Style['color-animated'], Style['hello-world']].join(' ')}><p>Hello World!! :)</p></span>
+
+            <span className={Style['sub-header']}><p>.~/$ cout &lt;&lt; whoami().condensed();</p></span>
+
+            <div className={Style['animated-typewriter']}>
+                <span><p>I'm Antonio</p></span>
+                <span><p>I was born {new Date().getFullYear() - 1991} years ago in the sunnny Apulia, Italy &#127774;🏖</p></span>
+            </div>
+            <div className={Style['animated-typewriter']}>
+                <span><p>I like creating stuff with code or bolts and screws.</p></span>
+            </div>
+            <div className={Style['animated-typewriter']}>
+                <span><p>I hope you like my projects, if you want to get in touch <a href="#contacts" className={Style['color-animated']}>connect with me!</a></p></span>
+            </div>
         </div>
     )
 
