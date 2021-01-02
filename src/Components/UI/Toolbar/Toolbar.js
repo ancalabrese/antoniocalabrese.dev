d91c83ba709f343a629d700a4e3ffc3b08bc4c12
Antonio <antoniobust@gmail.com>
2021-01-03 00:15:50 +0100

    Toggle toolbar shadow when scrolling

--- src/Components/UI/Toolbar/Toolbar.js
+++ src/Components/UI/Toolbar/Toolbar.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Style from './Toolbar.module.css'
 
 import Navigation from '../Navigation/Navigation'
 
-const Toolbar = (props) => {
+const Toolbar = () => {
+    const [showShadow, displayShadow] = useState(false);
+    const shadowThreshold = window.innerHeight - 5;
+    useEffect(() => {
+        function toggleShadow() {
+            const position = window.pageYOffset;
+            if (position > shadowThreshold && showShadow === false) {
+                displayShadow(true)
+            } else if (position < shadowThreshold && showShadow === true) {
+                displayShadow(false)
+            }
+        }
+        function watchScroll() {
+            window.addEventListener('scroll', toggleShadow)
+        }
+        watchScroll()
+        
+        return () => {
+            window.removeEventListener('scroll', toggleShadow);
+        }
+    });
+
     const scrollToTop = () => {
         window.scrollTo(0, 0);
     }
 
+
+
     return (
-        <div className={Style.toolbar}>
+        <div className={showShadow ? [Style.toolbar, Style['toolbar-shadow']].join(' ')
+            : Style.toolbar}>
             <div className={Style.profile_picture} onClick={scrollToTop} />
             <Navigation />
         </div>
